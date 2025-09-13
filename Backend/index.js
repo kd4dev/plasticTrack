704a8d1a64a57bafeea1c2bd90057f9bb6c87691
kedarBaswade <kedarbaswade2802@gmail.com>
2025-09-13 17:54:28 +0530

    Initial Frontend2

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT ?? 8000;
 connectMongoDb(process.env.MONGODB_URL).then(console.log("MongoDB connected"));
 
 app.use(express.json());
-
+app.get('/',async (req,res)=>{
+    return res.json({message:'MongoDb Connected'});
+})
 
 app.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
